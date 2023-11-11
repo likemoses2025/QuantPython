79a9a0a6da7c74fc0aed754110d76a0dd9be5401
likemoses2025 <mose233@gmail.com>
2023-11-11 17:03:32 +0900

    2

--- lesson/chapter3-8.py
+++ lesson/chapter3-8.py
@@ -1,8 +1,9 @@
 import seaborn as sns
+import pandas as pd
 
 #! 데이터 프레임에 함수 적용하기
 
-# ? 시리즈에 함수 적용하기
+# @ 시리즈에 함수 적용하기
 df = sns.load_dataset("penguins")
 print(df)
 #     species     island  bill_length_mm  bill_depth_mm  flipper_length_mm  body_mass_g     sex
@@ -27,7 +28,7 @@ print(bill_length_mm.head())
 # 4    36.7
 # Name: bill_length_mm, dtype: float64
 
-# ? 제곱근 구하기  sqrt함수는 제곱근을 구해줌
+# @ 제곱근 구하기  sqrt함수는 제곱근을 구해줌
 import numpy as np
 
 result = bill_length_mm.apply(np.sqrt)
@@ -52,4 +53,51 @@ print(result2.head())
 # 4    3.67
 # Name: bill_length_mm, dtype: float64
 
-#! P94부터
+# @ 데이터 프레임에 함수 적용하기
+df_num = df[["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]]
+print(df_num.head())
+#    bill_length_mm  bill_depth_mm  flipper_length_mm  body_mass_g
+# 0            39.1           18.7              181.0       3750.0
+# 1            39.5           17.4              186.0       3800.0
+# 2            40.3           18.0              195.0       3250.0
+# 3             NaN            NaN                NaN          NaN
+# 4            36.7           19.3              193.0       3450.0
+
+# * max - 열별 최대값
+print(df_num.apply(max))  # df_num.apply(max,axis=0)
+# bill_length_mm         59.6
+# bill_depth_mm          21.5
+# flipper_length_mm     231.0
+# body_mass_g          6300.0
+# dtype: float64
+
+# * max - 행별 최대값
+print(df_num.apply(max, axis=1))  # df_num.apply(max,axis=1)
+# 0      3750.0
+# 1      3800.0
+# 2      3250.0
+# 3         NaN
+# 4      3450.0
+#         ...
+# 339       NaN
+# 340    4850.0
+# 341    5750.0
+# 342    5200.0
+# 343    5400.0
+# Length: 344, dtype: float64
+
+
+# @ 데이터 프레임 열의 결측치 숫자
+def num_null(data):
+    null_vec = pd.isnull(data)
+    null_count = np.sum(null_vec)
+
+    return null_count
+
+
+print(df_num.apply(num_null))
+# bill_length_mm       2
+# bill_depth_mm        2
+# flipper_length_mm    2
+# body_mass_g          2
+# dtype: int64
---
+++ lesson/chapter3-9.py
@@ -0,0 +1,41 @@
+import seaborn as sns
+import pandas as pd
+
+#! 그룹 연산하기
+# 1. 분할 - split
+# 2. 적용 - apply
+# 3. 결합 - combine
+
+df = sns.load_dataset("penguins")
+print(df)
+#     species     island  bill_length_mm  bill_depth_mm  flipper_length_mm  body_mass_g     sex
+# 0    Adelie  Torgersen            39.1           18.7              181.0       3750.0    Male
+# 1    Adelie  Torgersen            39.5           17.4              186.0       3800.0  Female
+# 2    Adelie  Torgersen            40.3           18.0              195.0       3250.0  Female
+# 3    Adelie  Torgersen             NaN            NaN                NaN          NaN     NaN
+# 4    Adelie  Torgersen            36.7           19.3              193.0       3450.0  Female
+# ..      ...        ...             ...            ...                ...          ...     ...
+# 339  Gentoo     Biscoe             NaN            NaN                NaN          NaN     NaN
+# 340  Gentoo     Biscoe            46.8           14.3              215.0       4850.0  Female
+# 341  Gentoo     Biscoe            50.4           15.7              222.0       5750.0    Male
+# 342  Gentoo     Biscoe            45.2           14.8              212.0       5200.0  Female
+# 343  Gentoo     Biscoe            49.9           16.1              213.0       5400.0    Male
+
+# @ 그룹 나누기 - species에 따라 데이터의그룹을 나눔 groupby()
+
+df_group = df.groupby(["species"])
+print(df_group)
+# <pandas.core.groupby.generic.DataFrameGroupBy object at 0x165b04390>
+
+print(df_group.head(2))
+#        species     island  bill_length_mm  bill_depth_mm  flipper_length_mm  body_mass_g     sex
+# 0       Adelie  Torgersen            39.1           18.7              181.0       3750.0    Male
+# 1       Adelie  Torgersen            39.5           17.4              186.0       3800.0  Female
+# 152  Chinstrap      Dream            46.5           17.9              192.0       3500.0  Female
+# 153  Chinstrap      Dream            50.0           19.5              196.0       3900.0    Male
+# 220     Gentoo     Biscoe            46.1           13.2              211.0       4500.0  Female
+# 221     Gentoo     Biscoe            50.0           16.3              230.0       5700.0    Male
+
+for key, group in df_group:
+    print(key)
+    display(group.head(2))
